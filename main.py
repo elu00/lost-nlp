c4bc57b864a61531727d92fcc791ba1dd019420b
Ethan Lu <elu@nevada.unr.edu>
2018-11-25 23:35:07 -0500

    script

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import spacy
 
-nlp = spacy.load('en')
+nlp = spacy.load('en_core_web_lg')
+text = open('data/jinsun.txt', 'r').read() # open a document
+doc = nlp(text) # process it
+doc.to_disk('data/jinsun.bin') # save the processed Doc
