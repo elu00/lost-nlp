82defa47bd06b036e72938efdbdd913526edf171
Ethan Lu <elu@nevada.unr.edu>
2018-11-27 21:36:48 -0500

    update script

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import spacy
-
-nlp = spacy.load('en_core_web_lg')
-text = open('data/jinsun.txt', 'r').read() # open a document
-doc = nlp(text) # process it
-doc.to_disk('data/jinsun.bin') # save the processed Doc
+from textblob import TextBlob
+text = open('data/review1.txt', 'r').read() # open a document
+a = TextBlob(text)
